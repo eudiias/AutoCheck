1d168588532cddcd4472dbe67ccb2ded355831b5
Claudio Costa <costa.pederneschi@gmail.com>
2025-11-22 12:14:14 -0300

    modais cliente e veiculos funcionais

--- trabt2bd/js/dashboard.js
+++ trabt2bd/js/dashboard.js
@@ -99,3 +99,90 @@ async function carregarPlacas() {
     }
 }
 carregarPlacas();
+
+//select clientes
+
+const selectClientes = document.getElementById('selectClientes');
+
+async function carregarClientes() {
+    try {
+        const response = await fetch('https://fatecbackend.vercel.app/api/clientes/listar');
+        if(!response.ok){
+            throw new Error(`Erro na requisição: ${response.status}`);
+        }
+        const data = await response.json();
+        selectClientes.innerHTML = '<option value="">Selecione o cliente</option>';
+        data.forEach(func => {
+            const option = document.createElement('option');
+            option.value = func.cliente.id_cliente
+            option.textContent = func.nome_cliente;
+            selectClientes.appendChild(option);
+        });
+    } catch (error) {
+        console.error('Erro ao carregar os clientes registrados.', error);
+        selectClientes.innerHTML = '<option value="">Erro ao carregar</option>';
+    }
+}
+carregarClientes();
+
+// criar novo cliente
+
+document.getElementById('formNovoCliente').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const cliente = {
+        nome_cliente: document.getElementById('nomeCliente').value,
+        cpf_cliente: document.getElementById('cpfCliente').value,
+        telefone_cliente: document.getElementById('telCliente').value
+    };
+    await adicionarCliente(cliente);
+});
+// criar novo carro
+
+document.getElementById('formNovoCarro').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const carro = {
+        placa_carro: document.getElementById('num-placa').value,
+        id_cliente: await obterIdClientePorNome(document.getElementById('selectClientes').value)
+    };
+    await adicionarCarro(carro);
+});
+// criar nova triagem
+document.getElementById('formNovaTriagem').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    await adicionarTriagem();
+});
+async function adicionarCliente(cliente) {
+    try {
+        const response = await fetch('https://fatecbackend.vercel.app/api/clientes/adicionar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cliente)
+        });
+        if (!response.ok) {
+            throw new Error('Erro ao adicionar cliente');
+        }
+        alert('Cliente adicionado com sucesso!');
+        location.reload();
+    } catch (error) {
+        console.error('Erro ao adicionar cliente:', error);
+        alert('Erro ao adicionar cliente.');
+    }
+}
+// crie a parte de adicionar veiculos
+
+async function adicionarCarro(carro){
+    try{
+        const response = await fetch('https://fatecbackend.vercel.app/api/veiculos/adicionar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(carro)
+        });
+    } catch {
+        console.error('Erro ao adicionar Veiculo:',error)
+        alert('Erro ao adicionar veiculo')
+    }
+}
